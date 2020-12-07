0db3cc688ab2d5a57ac337fe8449276b374a2fef
trudge-64 <45760621+trudge-64@users.noreply.github.com>
2020-12-07 09:03:40 -0500

    Update Certificate.cs
    reinserted string RequireIndicatorEng

--- DataModel/Certificate.cs
+++ DataModel/Certificate.cs
@@ -21,6 +21,10 @@ namespace DataModel
         public string DescEng { get; set; }
 
         public string DescFre { get; set; }
+        
+        public string RequireIndicatorEng { get; set; }
+
+        public string RequireIndicatorFre { get; set; }
 
         //public virtual ICollection<JobPositionCertificate> JobPositionCertificates { get; set; }
 
