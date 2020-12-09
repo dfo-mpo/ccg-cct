c19c67b71665c8e6ef3ecd04a53392a1e3ba769e
drudn <56372323+drudn@users.noreply.github.com>
2020-12-09 16:01:29 -0500

    Dto fixes

--- Business.Queries/JobGroupLevels/GetAllJobGroupLevelsQueryHandler.cs
+++ Business.Queries/JobGroupLevels/GetAllJobGroupLevelsQueryHandler.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Business.Queries.Dtos;
+using Business.Dtos.JobGroups;
 using CCG.AspNetCore.Business.Interface;
 using DataModel;
 using Microsoft.EntityFrameworkCore;
--- Data.Classes/JobGroups/JobGroupPositionDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Data.Classes.JobGroups
-{
-    public class JobGroupPositionDto
-    {
-        public int JobId { get; set; }
-        public int LevelId { get; set; }
-        public string LevelValue { get; set; }
-    }
-}
