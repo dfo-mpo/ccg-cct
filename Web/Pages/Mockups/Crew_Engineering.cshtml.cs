e91d24fa12d36787450fd8ba375a0196880bbedb
Theresa Rudge <trudge@sympatico.ca>
2021-01-26 16:04:06 -0500

    Seeding database, changes to External pages and Resource files and image uploads

---
+++ Web/Pages/Mockups/Crew_Engineering.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages.Mockups
+{
+    public class Crew_EngineeringModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
