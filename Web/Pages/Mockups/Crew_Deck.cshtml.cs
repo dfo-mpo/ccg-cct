60d37de00f82b034d2d45cc9a2ac1722262884c8
trudge-64 <45760621+trudge-64@users.noreply.github.com>
2021-01-28 11:22:05 -0500

    Seeding database (#46)
    * Seeding database, changes to External pages and Resource files and image uploads
    
    * Seed Data

---
+++ Web/Pages/Mockups/Crew_Deck.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages.Mockups
+{
+    public class Crew_DeckModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
