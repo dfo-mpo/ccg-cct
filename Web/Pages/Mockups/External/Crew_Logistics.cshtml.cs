306c0a6050898ff3a362d41027a9d11ba30ade08
Theresa Rudge <trudge@sympatico.ca>
2021-02-17 16:39:00 -0500

    Update to SeedData, CCG Members Compare and Similar pages, Print layout. Updated pages for External and Student Mockups

---
+++ Web/Pages/Mockups/External/Crew_Logistics.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages.Mockups.External
+{
+    public class Crew_LogisticsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
