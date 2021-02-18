4733f0491dfdb55fbb95be198db644b8aec749f2
trudge-64 <45760621+trudge-64@users.noreply.github.com>
2021-02-18 08:10:25 -0500

    Update to SeedData, CCG Members Compare and Similar pages, Print layout. Updated pages for External and Student Mockups (#51)

---
+++ Web/Pages/Mockups/External/Crew_Deck.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages.Mockups.External
+{
+    public class Crew_DeckModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
