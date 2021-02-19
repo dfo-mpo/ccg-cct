b9f98b19e3908436a04971608d44e7819bc3b204
ccg-oharan <70717277+ccg-oharan@users.noreply.github.com>
2021-02-19 10:48:26 -0500

    Updated seed data helper to use execute strategy before transactions (#53)
    * enabling retry on failure
    
    * extract seed data methods to partial files
    
    * updated seed data add method to one generic
    
    * working

---
+++ DataModel/SeedData/SeedDataHelper.CompetencyTypes.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataModel.SeedData
+{
+    public partial class SeedDataHelper
+    {
+        public List<CompetencyType> CompetencyTypes = new List<CompetencyType>()
+        {
+            new CompetencyType() {Id = 1, NameEng = "Knowledge Elements", NameFre = "Éléments de connaissance"},
+            new CompetencyType() {Id = 2, NameEng = "Technical Elements", NameFre = "Éléments techniques"},
+            new CompetencyType() {Id = 3, NameEng = "Behavioural Elements", NameFre = "Éléments de comportement"}
+        };
+    }
+}
