5b35ddc649f14a8854100733e868c77994c843a8
ccg-oharan <70717277+ccg-oharan@users.noreply.github.com>
2021-03-04 10:50:57 -0500

    Admin site (#56)
    * created admin site

---
+++ Admin/Pages/Certificates/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DataModel;
+
+namespace Admin.Pages.Certificates
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DataModel.CctDbContext _context;
+
+        public IndexModel(DataModel.CctDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Certificate> Certificate { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Certificate = await _context.Certificates.ToListAsync();
+        }
+    }
+}
