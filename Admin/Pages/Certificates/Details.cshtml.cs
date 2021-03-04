cf0a813628ff2116b3d2f4479b277bd803723c2f
ccg-oharan <nicholas.o'hara@dfo-mpo.gc.ca>
2021-03-04 10:32:07 -0500

    admin site

---
+++ Admin/Pages/Certificates/Details.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DataModel;
+
+namespace Admin.Pages.Certificates
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly DataModel.CctDbContext _context;
+
+        public DetailsModel(DataModel.CctDbContext context)
+        {
+            _context = context;
+        }
+
+        public Certificate Certificate { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Certificate = await _context.Certificates.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Certificate == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
