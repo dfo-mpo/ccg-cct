0fd0c52e7f1bdd1ef5049285db5a3c648d920430
trudge-64 <45760621+trudge-64@users.noreply.github.com>
2021-03-05 16:12:28 -0500

    Update Student_List.cshtml.cs
    page location

--- Web/Pages/Mockups/Student/Student_List.cshtml.cs
+++ Web/Pages/Mockups/Student/Student_List.cshtml.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 
-namespace Web.Pages.Student
+namespace Web.Pages.Mockups.Student
 {
     public class Student_ListModel : PageModel
     {
