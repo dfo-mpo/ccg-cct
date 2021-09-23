8aad7db69e74536884cbf08863467b97d533cdc5
Danny Brisson <danny.brisson@dfo-mpo.gc.ca>
2021-09-23 15:21:23 -0400

    add localized routes

--- Web/Helpers/CultureTemplatePageRouteModelConvention.cs
+++ Web/Helpers/CultureTemplatePageRouteModelConvention.cs
@@ -1,11 +1,14 @@
-using System.Collections.Generic;
-using System.Linq;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using System.Collections.Generic;
+using System.Resources;
 
 namespace Web.Helpers
 {
     public class CultureTemplatePageRouteModelConvention : IPageRouteModelConvention
     {
+
+        private readonly ResourceManager _pageTranslations = Web.Resources.PageNames.ResourceManager;
+
         public void Apply(PageRouteModel model)
         {
             var selectorCount = model.Selectors.Count;
@@ -19,10 +22,27 @@ namespace Web.Helpers
                     AttributeRouteModel = new AttributeRouteModel
                     {
                         Order = -1,
-                        Template = AttributeRouteModel.CombineTemplates("{culture?}", selector.AttributeRouteModel.Template),
+                        Template = AttributeRouteModel.CombineTemplates("{culture?}", GetTranslation(selector.AttributeRouteModel.Template)),
                     }
                 });
             }
         }
+
+        private string GetTranslation(string template)
+        {
+            var templateSections = template.Split('/');
+            var translatedSections = new List<string>();
+            foreach (var section in templateSections)
+            {
+                translatedSections.Add(GetResource(section));
+            }
+            return string.Join("/", translatedSections);
+        }
+
+        private string GetResource(string section)
+ {
+            return _pageTranslations.GetString(section) ?? section;
+        }
+
     }
 }
